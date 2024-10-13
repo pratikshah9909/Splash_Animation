c86d276e5b424541adc49d6540b87d2d456ebb31
pratikshah9909 <141848462+pratikshah9909@users.noreply.github.com>
2024-10-13 12:01:13 +0530

    All Necessary files added.

---
+++ App.js
@@ -0,0 +1,24 @@
+// App.js
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from './src/screens/SplashScreen'; // Import your SplashScreen component
+import LoginScreen from './src/screens/LoginScreen';   // Import your LoginScreen component
+import HomeScreen from './src/screens/HomeScreen';      // Import your HomeScreen component
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SplashScreen">
+        <Stack.Screen name="SplashScreen" component={SplashScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
